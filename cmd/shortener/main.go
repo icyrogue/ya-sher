b104b2b52e87da791a69ee087983b2e42b564311
icy <saveapp1337@gmail.com>
2022-07-02 12:31:21 +0300

    Damn

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,22 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/icyrogue/ya-sher/handlers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "link shortner")
+	})
+
+	r.GET("/popa/", handlers.Popa())
+	r.POST("/", handlers.CrShort())
+	r.GET("/:id", handlers.ReLong())
+	r.Run()
+}
