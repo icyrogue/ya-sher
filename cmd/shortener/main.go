0606159bf368bc8c779b3d0d4c91743f93e9c0a5
icyrogue <89037640+icyrogue@users.noreply.github.com>
2022-07-03 20:23:19 +0300

    Revert "Damn"
    This reverts commit b104b2b52e87da791a69ee087983b2e42b564311.

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,22 +1,3 @@
 package main
 
-import (
-	"net/http"
-
-	"github.com/icyrogue/ya-sher/handlers"
-
-	"github.com/gin-gonic/gin"
-)
-
-func main() {
-	r := gin.Default()
-
-	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "link shortner")
-	})
-
-	r.GET("/popa/", handlers.Popa())
-	r.POST("/", handlers.CrShort())
-	r.GET("/:id", handlers.ReLong())
-	r.Run()
-}
+func main() {}
