a783a9481f15eb16502131243a02a29e0c91992f
icyrogue <89037640+icyrogue@users.noreply.github.com>
2022-07-08 01:07:07 +0300

    after rev

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"log"
+
 	"github.com/icyrogue/ya-sher/internal/api"
 	"github.com/icyrogue/ya-sher/internal/idgen"
-
-	"github.com/gin-gonic/gin"
+	"github.com/icyrogue/ya-sher/internal/urlstorage"
+	"go.uber.org/zap"
 )
 
 func main() {
 	idgen.InitID()
-	gin.SetMode(gin.ReleaseMode)
-	r := api.ApiInit()
-	r.Run()
-
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		log.Fatal(err)
+	}
+	storage := urlstorage.New()
+	usecase := idgen.New(storage)
+	api := api.New(logger, &api.Options{Hostname: "http:/localhost:8080"}, usecase, storage)
+	api.Init()
+	api.Run()
 }
